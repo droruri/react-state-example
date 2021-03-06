9fa4b12085f7550c58f969df2c8c0319f9b7b366
duri <duri@medallia.com>
2021-03-06 17:18:08 +0200

    stage 1

--- src/GradesTable.component.jsx
+++ src/GradesTable.component.jsx
@@ -1,25 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import {grades} from "./gradesStub";
-import {useState} from "react";
-import GradesHeader from "./components/GradesHeader.component";
-import StudentRow from "./components/StudentRow.component";
+import classNames from "classnames";
+import {cloneDeep} from "lodash";
+
 
 function GradesTable() {
-    const [tempGrades, /*setTempGrades*/] = useState(grades);
+    const [tempGrades, setTempGrades] = useState(grades);
     const rows = Object.entries(tempGrades.rows);
+
+    function updateGrade(studentName, index, grade){
+        const bla = cloneDeep(tempGrades);
+        bla.rows[studentName][index] = Number(grade);
+        setTempGrades(bla);
+    }
 
     return (
         <table>
             <thead>
-                <GradesHeader cols={tempGrades.columns}/>
+                <tr>
+                    <th></th>
+                    {tempGrades.columns.map(col => <th key={col}>{col}</th>)}
+                </tr>
             </thead>
             <tbody>
-            {rows.map(([name, grades]) =>
-                <StudentRow key={name} name={name} grades={grades}/>
-            )}
+            {rows.map(([name, grades]) => {
+                const avg = grades.reduce((a, b) => a + b) / grades.length;
+                return <tr key={name}>
+                    <td>{name}</td>
+                    {grades.map((grade, index) => <td key={index}>
+                        <input className={classNames({
+                            'bad-score': grade < 60,
+                            'mid-score': grade >= 60 && grade < 80,
+                            'good-score': grade >= 80
+                        })} defaultValue={grade} onChange={(ev)=>updateGrade(name, index, ev.target.value)} type="number" min={0} max={100}/>
+                    </td>)}
+                    <td>
+                        <div className={classNames({
+                            'bad-score': avg < 60,
+                            'mid-score': avg >= 60 && avg < 80,
+                            'good-score': avg >= 80
+                        })}>{avg}</div>
+                    </td>
+                </tr>
+            })}
             </tbody>
         </table>
     );
 }
 
 export default GradesTable;
+
+
+
+/*
+* pros: * no need to pass props to child components
+* cons: * pretty big component with lots of logic
+*       * non-readable template
+*       *
+* */
